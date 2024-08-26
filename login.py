5e9dd80aa410be03469f2e662c42c427b278bbeb
Sheri <esheri_slow@outlook.com>
2024-08-26 07:50:41 -0500

    Tkinter login screen
    Added code for Tkinter login screen. Included screen shot of finished project

---
+++ login.py
@@ -0,0 +1,25 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("login form")
+window.geometry('340x440')
+# window.configure(bg= '#333333')
+
+#creating the widget
+login_label = tkinter.Label(window, text="Login")
+username_label = tkinter.Label(window, text="Username")
+username_entry = tkinter.Entry(window)
+password_entry = tkinter.Entry(window, show="*")
+password_label = tkinter.Label(window, text="Password")
+login_button = tkinter.Button(window, text="Login")
+
+# Placing widgets on screen
+login_label.grid(row=0, column=0, columnspan=2)
+username_label.grid(row=1, column=0)
+username_entry.grid(row=1, column=1)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2, column=1)
+login_button.grid(row=3, column=0, columnspan=2)
+
+window.mainloop()
+print("hi")
